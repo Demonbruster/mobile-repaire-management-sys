93e532e2d99d030bf297a416f540401fe798fae1
demon <Demonbruster@gmail.com>
2023-01-07 20:25:43 +0530

    auth with iron session

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,10 @@ import styles from '../styles/Home.module.css'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import { useSession, signIn, signOut } from "next-auth/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const { data: session } = useSession()
-
-  const Auth = () => {
-    if (session) {
-      return <>
-        Signed in as {session?.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    }
-    return <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  }
-
 
   return (
     <>
@@ -33,7 +17,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Auth />
+        Hi
       </main>
     </>
   )
