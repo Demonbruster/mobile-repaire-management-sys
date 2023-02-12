9864fbc6fe83415b34bb0829c2756778e1d0563d
FM_Suhail <demonbruster@gmail.com>
2023-02-12 07:24:31 +0530

    dev: components

---
+++ components/common/Table/Table.tsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import { Box, Button, createStyles, Group, Stack, Text, useMantineTheme } from '@mantine/core';
+import { closeAllModals, openModal } from '@mantine/modals';
+import { showNotification } from '@mantine/notifications';
+import { DataTable, DataTableSortStatus } from 'mantine-datatable';
+import { useState } from 'react';
+import { IconEdit, IconTrash, IconTrashX } from '@tabler/icons-react';
+
+export default function Table() {
+  return (
+    <div>
+      <h1>Table</h1>
+    </div>
+  );
+}
