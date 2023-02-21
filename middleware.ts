0a20433511b4e5c8a7add1702651e602f831a4e3
FM_Suhail <demonbruster@gmail.com>
2023-02-21 12:30:28 +0530

    fix: check auth credentials and redirection

--- middleware.ts
+++ middleware.ts
@@ -3,42 +3,51 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getIronSession } from "iron-session/edge";
 
+const ENV = {
+	cookieName: process.env.IRON_COOKIE_NAME || "myapp_cookiename",
+	password: process.env.IRON_COOKIE_PASSWORD || "complex_password_at_least_32_characters_long",
+};
+
 export const middleware = async (req: NextRequest) => {
-  const res = NextResponse.next();
-  const session = await getIronSession(req, res, {
-    cookieName: "myapp_cookiename",
-    password: "complex_password_at_least_32_characters_long",
-    // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-    cookieOptions: {
-      secure: process.env.NODE_ENV === "production",
-    },
-  });
+	const res = NextResponse.next();
+	const session = await getIronSession(req, res, {
+		...ENV,
+		// secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
+		cookieOptions: {
+			secure: process.env.NODE_ENV === "production",
+		},
+	});
 
-  // do anything with session here:
-  const { user } = session; 
+	// do anything with session here:
+	const { user } = session;
 
+	// like mutate user:
+	// user.something = someOtherThing;
+	// or:
+	// session.user = someoneElse;
 
-  // like mutate user:
-  // user.something = someOtherThing;
-  // or:
-  // session.user = someoneElse;
+	// uncomment next line to commit changes:
+	// await session.save();
+	// or maybe you want to destroy session:
+	// await session.destroy();
 
-  // uncomment next line to commit changes:
-  // await session.save();
-  // or maybe you want to destroy session:
-  // await session.destroy();
+	console.log("from middleware", { user });
 
-  console.log("from middleware", { user });
+	// demo:
+	if (user?.admin !== true) {
+		// unauthorized to see pages inside admin/
+		return NextResponse.redirect(new URL("/", req.url)); // redirect to /unauthorized page
+	}
 
-  // demo:
-  if (user?.admin !== true) {
-    // unauthorized to see pages inside admin/
-    return NextResponse.redirect(new URL('/', req.url)) // redirect to /unauthorized page
-  }
+	// if already logged in, redirect to /admin
+	if (user?.admin === true) {
+		// redirect to /admin page
+		return NextResponse.redirect(new URL("/dashboard", req.url));
+	}
 
-  return res;
+	return res;
 };
 
 export const config = {
-  matcher: "/admin",
+	matcher: "/",
 };
