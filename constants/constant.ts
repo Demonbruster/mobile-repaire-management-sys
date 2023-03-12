3081d2e74ae8e09ced0c9aa78667766a23d49d33
FM_Suhail <demonbruster@gmail.com>
2023-03-12 09:28:40 +0530

    dev: constant

---
+++ constants/constant.ts
@@ -0,0 +1,14 @@
+export const reactQueryKey = {
+  devices : 'DEVICES',
+  device : 'DEVICE',
+  brands : 'BRANDS',
+  models : 'MODELS',
+  repairers : 'REPAIRERS',
+  repairer : 'REPAIRER',
+  customers : 'CUSTOMERS',
+  login : 'LOGIN',
+} as const;
+
+export const sizes = {
+  FOOTER_ICON_SIZE : 34,
+}
