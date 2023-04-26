f4d47086c8f46adc3c37727c8ec2ecc2863c80fd
FM_Suhail <demonbruster@gmail.com>
2023-04-26 11:07:45 +0530

    dev: repairer table with search bar

--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -1,12 +1,11 @@
-import React, { useMemo } from 'react'
-import { useMediaQuery } from '@mantine/hooks';
-import { ActionIcon, AppShell, Flex, Header, Text, Box, Skeleton } from '@mantine/core'
-import { IconUsers, IconDeviceMobile, IconDeviceMobileVibration } from '@tabler/icons-react'
+import React, { useMemo, useState } from 'react'
+import { useDebouncedValue, useMediaQuery } from '@mantine/hooks';
+import { ActionIcon, AppShell, Flex, Text, Box, Skeleton, Card, Title, Grid, Group, TextInput, Button, Stack } from '@mantine/core'
+import { IconUsers, IconDeviceMobile, IconDeviceMobileVibration, IconSearch, IconCaretDown, IconCaretUp } from '@tabler/icons-react'
 import { DataTable } from 'mantine-datatable';
 
 import { reactQueryKey, sizes } from '../../constants/constant';
 import NewRepairer from '../../components/container/repairer/NewRepairer';
-import NewModel from '../../components/container/model/NewModel';
 import { useQuery } from 'react-query';
 import { getRepairers } from '../../endpoints/repairer';
 
@@ -35,32 +34,32 @@ const Dashboard = () => {
     <AppShell
       footer={
         isMobile ?
-        <Box sx={{
-          position: 'fixed',
-          bottom: 0,
-          height: 80,
-          width: '100%',
-          boxShadow: '0px -2px 4px rgba(0, 0, 0, 0.1)',
-          borderTop: '1px solid #E5E5E5',
-        }}
+          <Box sx={{
+            position: 'fixed',
+            bottom: 0,
+            height: 80,
+            width: '100%',
+            boxShadow: '0px -2px 4px rgba(0, 0, 0, 0.1)',
+            borderTop: '1px solid #E5E5E5',
+          }}
 
-        >
-          <Flex justify="space-evenly" align="center"  >
-            {list.map((data, index) => <Box py={5} key={index}>
-              <ActionIcon
-                variant="outline"
-                size="xl"
-                color="teal"
-              >
-                {data.icon}
-              </ActionIcon>
-              <Text c="dimmed" fz="xs" fs='italic'> {data.name} </Text>
-            </Box>)}
-          </Flex>
-        </Box> : <></>
+          >
+            <Flex justify="space-evenly" align="center"  >
+              {list.map((data, index) => <Box py={5} key={index}>
+                <ActionIcon
+                  variant="outline"
+                  size="xl"
+                  color="teal"
+                >
+                  {data.icon}
+                </ActionIcon>
+                <Text c="dimmed" fz="xs" fs='italic'> {data.name} </Text>
+              </Box>)}
+            </Flex>
+          </Box> : <></>
       }
     >
-      <RepairerTable/>
+      <RepairerTable />
     </AppShell>
   )
 }
@@ -68,16 +67,28 @@ const Dashboard = () => {
 export default Dashboard
 
 function RepairerTable() {
-  const { isLoading, data, isError, error }  = useQuery(reactQueryKey.repairers, async () => getRepairers())
+  const { isLoading, data, isError, error } = useQuery(reactQueryKey.repairers, async () => getRepairers())
+
+  const [isNewRepairerOpen, setIsNewRepairerOpen] = useState(false)
+  const [query, setQuery] = useState('');
+  const [debouncedQuery] = useDebouncedValue(query, 200);
 
   const records = useMemo(() => {
-    return data?.repairers?.map((repairer:any) => ({
-      device: repairer.device?.name ,
+    const tunedData = data?.repairers?.map((repairer: any) => ({
+      device: repairer.device?.name,
       customer: repairer.customer?.phone + ' ' + (repairer.customer.name ? repairer.customer.name : ''),
       problem: repairer.problem,
       status: repairer.status
     }))
-  }, [data])
+
+    if (!debouncedQuery) return tunedData;
+
+    return tunedData?.filter((record: any) => {
+      return Object.keys(record).some((key) => {
+        return String(record[key]).toLowerCase().includes(debouncedQuery.toLowerCase());
+      });
+    });
+  }, [data, debouncedQuery])
 
 
   const columns = [
@@ -93,18 +104,55 @@ function RepairerTable() {
     {
       accessor: 'status'
     }
-  ] 
+  ]
 
-  if(isLoading) return <Skeleton height={loadingSize} />
+  if (isLoading) return <Skeleton height={loadingSize} />
 
-  return <DataTable 
-    columns={columns} 
-    records={records}
-    striped
-    highlightOnHover 
-    key={
-      records?.map((record:any) => record.device + record.customer + record.problem + record.status).join('')
-    }
-  />
-
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder p='lg'>
+      <Stack>
+        <Grid align="center" mb="md">
+          <Grid.Col span={6}>
+            <Title order={3} mb={5}>Repairers</Title>
+          </Grid.Col>
+          <Grid.Col span={6}>
+            <Group position='right'>
+              <Flex direction='row' gap='md' sx={{ flexBasis: '100%' }} >
+                <TextInput
+                  sx={{ flexBasis: '70%' }}
+                  placeholder="Search..."
+                  value={query}
+                  onChange={(event) => setQuery(event.currentTarget.value)}
+                  icon={<IconSearch size={16} />}
+                />
+                <Button variant="filled" sx={{ flexBasis: '30%' }}
+                  leftIcon={
+                    isNewRepairerOpen ? <IconCaretUp size={16} /> : <IconCaretDown size={16} />
+                  }
+                  onClick={() => setIsNewRepairerOpen((prev) => !prev)}
+                >
+                  {isNewRepairerOpen ? 'Close' : 'New'}
+                </Button>
+              </Flex>
+            </Group>
+          </Grid.Col>
+        </Grid>
+        {isNewRepairerOpen &&
+          <Card withBorder >
+            <NewRepairer />
+          </Card>
+        }
+        <DataTable
+          minHeight={loadingSize}
+          columns={columns}
+          records={records}
+          striped
+          highlightOnHover
+          key={
+            records?.map((record: any) => record.device + record.customer + record.problem + record.status).join('')
+          }
+        />
+      </Stack>
+    </Card>
+  )
 }
